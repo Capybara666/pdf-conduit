edeb3c7466270b66c94784e2b5b198e4a44ba755
Capybara666 <damian.niemczyk314@gmail.com>
2026-06-15 21:48:41 +0200

    fix(core): use nested records in PageSource sealed interface

--- pdf-utils-core/src/main/java/org/example/core/model/PageSource.java
+++ pdf-utils-core/src/main/java/org/example/core/model/PageSource.java
@@ -2,8 +2,9 @@ package org.example.core.model;
 
 import java.nio.file.Path;
 
-public sealed interface PageSource permits PdfPageSource, ImageSource {}
+public sealed interface PageSource permits PageSource.PdfPageSource, PageSource.ImageSource {
 
-record PdfPageSource(Path file, PageRange range) implements PageSource {}
+    record PdfPageSource(Path file, PageRange range) implements PageSource {}
 
-record ImageSource(Path file, PageSize targetSize) implements PageSource {}
+    record ImageSource(Path file, PageSize targetSize) implements PageSource {}
+}
